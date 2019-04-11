647f2bb4c449fe7387316f90aec13cd4df8f6458
Patricio Jaime <patricio.jaime@tigomx.com>
2019-04-10 23:48:24 -0500

    Añadido Transparencia

--- Shaders/texturizado.fs
+++ Shaders/texturizado.fs
@@ -8,4 +8,8 @@ void main(){
     //Cambiamos el color por la textura
     color = texture(ourTexture, out_tex);
     //color = vec4(0.3, 0.5, 0.4, 1.0);
+    if(color.a <= 0.1){
+        discard;
+    }
+
 }
